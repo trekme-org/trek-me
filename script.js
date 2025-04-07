bffacde4d6f5a231258a208843f2f7c07b0b6316
Saqibsarwar12 <hasimtariq55@gmail.com>
2025-04-07 21:23:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const galleryItems = document.querySelectorAll('.gallery img, .gallery video');
-  galleryItems.forEach(item => {
-    item.addEventListener('click', () => {
-      alert('Thanks for checking out our trek media!');
-    });
-  });
-});
+{
+  "hosting": {
+    "public": "public",
+    "rewrites": [
+      {
+        "source": "**",
+        "destination": "/index.html"
+      }
+    ]
+  }
+}
